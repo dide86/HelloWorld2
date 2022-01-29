28e56ada86a4341388b0bb40d9b1d455ec9212e8
dide <justgamegeorge@gmail.com>
2022-01-29 16:13:16 +0500

    добавили нахождение максимального числа

---
+++ Example006_Max/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 4;
+int c = 9;
+int d = 7;
+int f = 8;
+
+int max = a;
+
+if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;
+if(d>max) max = d;
+if(f>max) max = f;
+Console.WriteLine("Максимальное число - " + max);
