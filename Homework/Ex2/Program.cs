0df0430b2b3909364a9c6dfb60f90bd5e8365569
dide <justgamegeorge@gmail.com>
2022-02-07 15:52:22 +0500

    Сделал 2 задание

---
+++ Homework/Ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2:** Даны два числа. Показать большее и меньшее число
+int a = 7;
+int b = 9;
+int max = a;
+int min = b;
+if(b>max) {
+    max = b;
+    min = a;
+    }
+Console.WriteLine("Максимальное число - " + max);
+
+Console.WriteLine("Минимальное число - " + min);
