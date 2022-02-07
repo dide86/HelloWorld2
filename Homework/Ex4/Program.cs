9b0d6b96f0863f2ee76d7219edba54da0004cb21
dide <justgamegeorge@gmail.com>
2022-02-07 15:55:08 +0500

    Сделал 4 задание

---
+++ Homework/Ex4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 4:** Найти максимальное из трёх чисел.
+int a = 1;
+int b = 4;
+int c = 9;
+
+int max = a;
+
+if(b>max) max = b;
+if(c>max) max = c;
+Console.WriteLine("Максимальное число - " + max);
