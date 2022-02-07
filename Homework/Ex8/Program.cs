f8f2150488c6a9c1227660c1df0876a8ed511405
dide <justgamegeorge@gmail.com>
2022-02-07 16:22:32 +0500

    Улучшил код

--- Homework/Ex8/Program.cs
+++ Homework/Ex8/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Задача 8:** Показать чётные числа от 1 до N.
 int N = 14;
-int[] array = new int[N + 1];
+int[] array = new int[N+1];
 int index = 0;
 Console.WriteLine("Четные числа от 1 до " + N);
-while (index < N)
+while (index < array.Length)
 {
-    index++;
+
     array[index] = index;
 
     if (array[index] % 2 == 1)
@@ -16,5 +16,5 @@ while (index < N)
     {
         Console.WriteLine(array[index]);
     }
-
+    index++;
 }
