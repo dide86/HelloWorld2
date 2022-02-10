487d0f547f3d391a5bb3b53c3fe8ad471cf8046c
dide <justgamegeorge@gmail.com>
2022-02-10 14:21:30 +0500

    Сделал 16 задачу

---
+++ Homework/Ex16/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 16:** Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+int a = new Random().Next(1, 8);
+Console.WriteLine(a);
+if (a == 6) { Console.WriteLine(a + " - Суббота"); }
+
+else if (a == 7) { Console.WriteLine(a + " - Воскресенье"); }
+
+else { Console.WriteLine(a + " - рабочий день"); };
