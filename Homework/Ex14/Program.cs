8a3536ace94c8b4e39d33ade3be3f0bd09316828
dide <justgamegeorge@gmail.com>
2022-02-10 14:22:05 +0500

    14 задачу попробовал, но запутался

---
+++ Homework/Ex14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 14:** Найти третью цифру числа или сообщить, что её нет.
+using static System.Console;
+WriteLine("Напишите число и я найду 3ю цифру");
+int number = int.Parse(ReadLine());
+int [] size = {1, 2, 3};
+if (number < size.Length)
+{
+    WriteLine("В числе нету 3 цифры");
+}
+else
+{
+    string third = (int.Parse(number[2].ToString()));
+    WriteLine("3я цифра - " + third);
+}
