eb8cae598a1cf9d686f9afafe4082f7c689ba1b9
dide <justgamegeorge@gmail.com>
2022-02-10 13:20:59 +0500

    сделал 12 задачу

---
+++ Homework/Ex12/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 12:** Удалить вторую цифру трёхзначного числа.
+using static System.Console;
+WriteLine("Напишите трехзначное число и я удалю второе - ");
+string a = ReadLine();
+a = (a[0].ToString()) + (a[2].ToString());
+WriteLine(a);
