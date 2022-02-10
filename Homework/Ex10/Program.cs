411fa5ca7f555a7a0ad83c4ccc64785254800b83
dide <justgamegeorge@gmail.com>
2022-02-10 13:12:49 +0500

    Сделал 10 задание

---
+++ Homework/Ex10/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 10:** Показать вторую цифру трёхзначного числа.
+using static System.Console;
+WriteLine("Напишите трехзначное число и я покажу второе - ");
+string a = ReadLine();
+WriteLine(int.Parse(a[1].ToString()));
+
