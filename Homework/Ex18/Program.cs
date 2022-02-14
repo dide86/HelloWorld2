c32eb259b92bf7e7f451bb2452edd712dc6a663f
dide <justgamegeorge@gmail.com>
2022-02-14 16:41:10 +0500

    добавил 18 задание

---
+++ Homework/Ex18/Program.cs
@@ -0,0 +1,31 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+using static System.Console;
+int x = 0;
+int y = 0;
+
+if (-x * -y == -(x * y))
+{
+    WriteLine("Выражение истено x = 0, y = 0");
+}
+else WriteLine("Выражение не истено x = 0, y = 0");
+x = 0;
+y = 1;
+if (-x * -y == -(x * y))
+{
+    WriteLine("Выражение истено x = 0, y = 1");
+}
+else WriteLine("Выражение не истено x = 0, y = 1");
+x = 1;
+y = 0;
+if (-x * -y == -(x * y))
+{
+    WriteLine("Выражение истено x = 1, y = 0");
+}
+else WriteLine("Выражение не истено x = 1, y = 0");
+x = 1;
+y = 1;
+if (-x * -y == -(x * y))
+{
+    WriteLine("Выражение истено x = 1, y = 1");
+}
+else WriteLine("Выражение не истено  x = 1, y = 1");
