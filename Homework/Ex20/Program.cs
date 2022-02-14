78a0706dacfd152fc8d429f852c0d0cf47912b84
dide <justgamegeorge@gmail.com>
2022-02-14 16:51:11 +0500

    добавил 20 задание

---
+++ Homework/Ex20/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+using static System.Console;
+int number = int.Parse(ReadLine());
+if (number == 1)
+{
+    WriteLine("Диапазон координат для 1ой четверти x > 0 , y > 0 ");
+}
+else if (number == 2)
+{
+    WriteLine("Диапазон координат для 2ой четверти x < 0 , y > 0 ");
+}
+else if (number == 3)
+{
+    WriteLine("Диапазон координат для 3ой четверти x < 0 , y < 0");
+}else if (number == 4){
+    WriteLine("Диапазон координат для 4ой четверти x > 0 , y < 0 ");
+}else {
+    WriteLine("Неверно указана четверть");
+}
