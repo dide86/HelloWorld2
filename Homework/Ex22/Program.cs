fd1a27fa5a633a1be37b4bf38e6dfeebf0630bb1
dide <justgamegeorge@gmail.com>
2022-02-14 17:06:35 +0500

    добавил 22 задание

---
+++ Homework/Ex22/Program.cs
@@ -0,0 +1,8 @@
+﻿//Найти расстояние между точками в пространстве 2D/3D
+using static System.Console;
+int[] a = { 1, 1 };
+int[] b = { 5, 4 };
+double road = 0;
+road = ((Math.Pow(b[0], 2) - Math.Pow(a[0], 2) + (Math.Pow(b[1], 2) - Math.Pow(a[1], 2))));
+road = Math.Sqrt(road);
+WriteLine("Расстояние между точками равно = " + road);
