cf7dcbe1d36324ee98190d856c55374f62ff358d
dide <justgamegeorge@gmail.com>
2022-02-14 17:11:55 +0500

    добавил 24 задание

---
+++ Homework/Ex24/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+using static System.Console;
+int number = int.Parse(ReadLine());
+int index = 0;
+double score = 0;
+while (index<number) {
+    index++;
+    score = index*index*index;
+    WriteLine($"Куб числа {index} равен {score}");
+}
