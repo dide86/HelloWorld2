df4644cc5e7cc614a1974e5be39bf451d11dea63
dide <justgamegeorge@gmail.com>
2022-02-17 19:11:37 +0500

    сделал 28 задачу

---
+++ Homework/Ex28/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 28: Подсчитайте сумму цифр в числе.
+using static System.Console;
+string number = (ReadLine());
+int[] array = number.ToString().Select(c=>(int)char.GetNumericValue(c)).ToArray();
+int result = array.Sum();
+WriteLine(result);
