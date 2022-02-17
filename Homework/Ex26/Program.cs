a67c6c3fbf69aacd6ac7744e0ae710c8200ff868
dide <justgamegeorge@gmail.com>
2022-02-17 18:52:11 +0500

    сделал 26 задачу

---
+++ Homework/Ex26/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 26: Возведите число А в натуральную степень B, используя цикл.
+using static System.Console;
+int number = 5;
+int step = 1;
+int index = 1;
+int result = number;
+while ( index < step) {
+result = result * number;
+index++;
+}
+WriteLine(result);
